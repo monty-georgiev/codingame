36519d39c6e044548c01b9dfc8fbfa119fa2c77f
monty-georgiev <monty.georgiev@gmail.com>
2018-07-08 18:46:27 +0000

    Adding temperatures solution

---
+++ easy/temperatures.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+n = int(input())
+
+tempMax = 5526
+tempMin = -273
+closest = 0
+
+
+for i in input().split():
+
+    t = int(i) #t :temperature expressed as an integer between -273 an 5526
+
+    if t < tempMax and t > 0:
+        tempMax = t
+        closest = t
+    elif t > tempMin and t < 0:
+        tempMin = t
+
+        if abs(t) < tempMax:
+            closest = t
+
+print(closest)
